e55d1baad3e9c8ecf2b3821f835c01f9fc300600
hfaria <hfaria@bhz01cpi03.fitec.org.br>
2017-11-28 09:23:21 -0200

    mudança de nome de arquivo

--- test/br/org/fitec/cpi/tdd/ex1/MyCalculatorTestAdd.java
+++ test/br/org/fitec/cpi/tdd/ex1/MyCalculatorTestAdd.java
@@ -21,7 +21,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class MyCalculatorTest {
+public class MyCalculatorTestAdd {
 
 	Calculator myCalculator;
 
--- test/br/org/fitec/cpi/tdd/ex1/MyCalculatorTestDivide.java
+++ test/br/org/fitec/cpi/tdd/ex1/MyCalculatorTestDivide.java
@@ -21,7 +21,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class MyCalculatorTestDividir {
+public class MyCalculatorTestDivide {
 
 	Calculator myCalculator;
 
