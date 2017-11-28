01f7a77b57d1d95c5fdd76cc6aec740180d0e335
hfaria <hfaria@bhz01cpi03.fitec.org.br>
2017-11-28 08:45:57 -0200

    melhor organização do codigo

--- test/br/org/fitec/cpi/tdd/ex1/MyCalculatorTest.java
+++ test/br/org/fitec/cpi/tdd/ex1/MyCalculatorTest.java
@@ -45,26 +45,16 @@ public class MyCalculatorTest {
 	}
 
 	@Test
-	public void addStringVaziaResultado0() {
+	public void addStringInvalidResult0() {
 		Assert.assertThat(0, equalTo(myCalculator.add("")));
-	}
-	
-	@Test
-	public void addStringVaziaResultado02() {
 		Assert.assertThat(0, equalTo(myCalculator.add(",")));
 		Assert.assertThat(0, equalTo(myCalculator.add(",,,")));
 		Assert.assertThat(0, equalTo(myCalculator.add(", , , ")));
-	}
-	
-
-
-	@Test
-	public void addStringNullaResultado0() {
 		Assert.assertThat(0, equalTo(myCalculator.add(null)));
 	}
 
 	@Test
-	public void addStringUmUnicaEntradaResultadoRecebido() {
+	public void addStringUniqueValueUsedWithResult() {
 		Assert.assertThat(0, equalTo(myCalculator.add("0")));
 		Assert.assertThat(1, equalTo(myCalculator.add("1")));
 		Assert.assertThat(2, equalTo(myCalculator.add("2")));
@@ -72,31 +62,11 @@ public class MyCalculatorTest {
 	}
 
 	@Test
-	public void addStringUmUnicaEntradaComValoresFracionadosResultadoRecebido() {
-		Assert.assertThat(0, equalTo(myCalculator.add("0")));
-		Assert.assertThat(1, equalTo(myCalculator.add("1")));
-		Assert.assertThat(2, equalTo(myCalculator.add("2")));
-		Assert.assertThat(3, equalTo(myCalculator.add("3")));
-	}
-
-	@Test
-	public void addStringDiferenteResultado() {
-		Assert.assertNotEquals(1, equalTo(myCalculator.add("0")));
-		Assert.assertNotEquals(2, equalTo(myCalculator.add("1")));
-		Assert.assertNotEquals(3, equalTo(myCalculator.add("2")));
-		Assert.assertNotEquals(4, equalTo(myCalculator.add("3")));
-	}
-
-	@Test
-	public void addSoma2itensResultadoSomaDosDois() {
+	public void addSum2ItensWithResult() {
 		Assert.assertThat(1, equalTo(myCalculator.add("0,1")));
 		Assert.assertThat(1, equalTo(myCalculator.add("1,0")));
 		Assert.assertThat(2, equalTo(myCalculator.add("1,1")));
 		Assert.assertThat(3, equalTo(myCalculator.add("1,2")));
-	}
-
-	@Test
-	public void addSoma3itensResultadoDiferente() {
 		Assert.assertNotEquals(0, equalTo(myCalculator.add("0,1")));
 		Assert.assertNotEquals(2, equalTo(myCalculator.add("0,1")));
 		Assert.assertNotEquals(0, equalTo(myCalculator.add("1,0")));
@@ -106,7 +76,7 @@ public class MyCalculatorTest {
 	}
 
 	@Test
-	public void addSoma3itensResultadoSomaDosDois() {
+	public void addSun3itensWithResult() {
 		Assert.assertThat(3, equalTo(myCalculator.add("0,1,2")));
 		Assert.assertThat(3, equalTo(myCalculator.add("1,0,2")));
 		Assert.assertThat(4, equalTo(myCalculator.add("1,1,2")));
@@ -114,7 +84,7 @@ public class MyCalculatorTest {
 	}
 
 	@Test
-	public void addStringVazia2ResultadoUnicoValor() {
+	public void addWithEmptyValuesWithResult() {
 		Assert.assertThat(1, equalTo(myCalculator.add(",1")));
 		Assert.assertThat(1, equalTo(myCalculator.add(",1")));
 		Assert.assertThat(1, equalTo(myCalculator.add("1, ")));
@@ -122,39 +92,29 @@ public class MyCalculatorTest {
 		Assert.assertThat(3, equalTo(myCalculator.add("1,2, ")));
 		Assert.assertThat(3, equalTo(myCalculator.add("1, , 2")));
 		Assert.assertThat(3, equalTo(myCalculator.add(" , 1 , 2")));
-	}
-
-	@Test
-	public void addStringVazia2ResultadoUnicoValos() {
-		thrown.expectMessage(is("negatives not allowed"));
-		myCalculator.add(", -1 , ");
-	}
-	
-	@Test
-	public void add4valores() {
 		Assert.assertThat(10, equalTo(myCalculator.add("1 , 2 , 3, 4")));
 		Assert.assertThat(8, equalTo(myCalculator.add("1 ,  , 3, 4")));
 		Assert.assertThat(10, equalTo(myCalculator.add("1 ,  , 3, 4, 2")));
 	}
 
 	@Test
-	public void notAllowed(){
+	public void addWithEmptyValueAndInvalidValueWithException() {
+		thrown.expectMessage(is("negatives not allowed"));
+		myCalculator.add(", -1 , ");
+	}
+	
+	@Test
+	public void addWithInvalidValueWithException(){
 		thrown.expect(NegativeNumberException.class);
 		thrown.expectMessage(is("negatives not allowed"));
 		myCalculator.add("-1,3");
-	}
-
-	@Test
-	public void notAllowed2() {
+	
 		thrown.expect(NegativeNumberException.class);
 		thrown.expectMessage(containsString("-1"));
 		thrown.expectMessage(containsString("-3"));
 		thrown.expectMessage(is("negatives not allowed -1 -3"));
 		myCalculator.add("-1,-3");
-	}
 
-	@Test
-	public void notAllowed3() {
 		thrown.expect(NegativeNumberException.class);
 		thrown.expectMessage(containsString("-1"));
 		thrown.expectMessage(containsString("-3"));
@@ -164,26 +124,23 @@ public class MyCalculatorTest {
 	}
 	 
 	@Test
-	public void substractNumberFormatException(){
+	public void addWithUnparsedWithException(){
 		thrown.expect(NumberFormatException.class);
 		thrown.expectMessage(is("It was not possible to parse " + "asdfsdf"));
 		myCalculator.add("-1,asdfsdf");
-	}
-	
-	@Test
-	public void subtrairSoma3itensResultadoDiferentes() {
-		Assert.assertEquals(myCalculator.add("1,2"), myCalculator.add("2,1"));
-	}
-	
-	@Test
-	public void substractNumberFormatException2(){
+		
 		thrown.expect(NumberFormatException.class);
 		thrown.expectMessage(is("It was not possible to parse -asdf1"));
 		myCalculator.add("-asdf1,asdfsdf");
 	}
-
+	
 	@Test
-	public void addBiggerThan1000() {
+	public void addWithTestSimileOperation() {
+		Assert.assertEquals(myCalculator.add("1,2"), myCalculator.add("2,1"));
+	}
+	
+	@Test
+	public void addBiggerThan1000WithResult() {
 		Assert.assertThat(1, equalTo(myCalculator.add("1,1002")));
 		Assert.assertThat(1, equalTo(myCalculator.add("1,1001")));
 		Assert.assertThat(1001, equalTo(myCalculator.add("1,1000")));
